a53d532e165feb605334fa3172bc6d58b72e8343
Sade Stevens <sadestevens.hc@gmail.com>
2015-03-16 12:04:16 -0400

    paired on app up to this point, through Sage's repo. Forked. Now working on seed file

--- travel/db/seeds.rb
+++ travel/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+User.create([
+  { first_name: "Angelina", last_name: "Bethoney", username: "female", email: "ambethoney@yahoo.com", password: "password"},
+  { first_name: "Sarah", last_name: "McAlear", username: "female", email: "sarahm@yahoo.com", password: "password"},
+  { first_name: "Brian", last_name: "Rucker", username: "male", email: "brucker@gmail.com", password: "password"}
+])
+
+
+
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
