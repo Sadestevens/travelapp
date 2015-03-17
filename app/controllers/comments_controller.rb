6fd600b45465375166fe6d021b8b618d1211b62b
Sade Stevens <sadestevens.hc@gmail.com>
2015-03-17 14:50:02 -0400

    reinitialized

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.json { render json: @comment }
+    end
+  end
+
+  def index
+    @all_comments = Comment.all
+    respond_to do |format|
+      format.json { render json: @all_comments }
+    end
+  end
+
+  def suggestion_comments
+    @comments = Comment.where(suggestion_id: params[:id]).all
+    respond_to do |format|
+      format.json { render json: @comments }
+    end
+  end
+
+  private
+
+  def comment_params
+    @comment_params ||= params.require(:comment).permit(:suggestion_id, :user_id, :content)
+  end
+
+end
