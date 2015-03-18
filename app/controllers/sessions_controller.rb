7cad6e310aafe1bed83e132083cab6d92ebd6dc6
Sade Stevens <sadestevens.hc@gmail.com>
2015-03-18 12:06:06 -0400

    added in omniauth info and added to the create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,10 @@ class SessionsController < ApplicationController
 
   def create
     @auth = request.env['omniauth.auth']['credentials']
+    Token.create(
+      access_token: @auth['token'],
+      refresh_token: @auth['refresh_token'],
+      expires_at: Time.at(@auth['expires_at']).to_datetime)
 
     @user = User.find_by(email: session_params[:email])
     if @user && @user.authenticate(session_params[:password])
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316233950) do
+ActiveRecord::Schema.define(version: 20150318160403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,14 @@ ActiveRecord::Schema.define(version: 20150316233950) do
 
   add_index "suggestions", ["category_id"], name: "index_suggestions_on_category_id", using: :btree
   add_index "suggestions", ["user_id"], name: "index_suggestions_on_user_id", using: :btree
+
+  create_table "tokens", force: :cascade do |t|
+    t.string   "access_token"
+    t.string   "refresh_token"
+    t.datetime "expires_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.integer  "group_id"
