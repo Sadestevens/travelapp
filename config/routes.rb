626b755ffc97bb33b8c6dc7b88f163eac6001446
Sade Stevens <sadestevens.hc@gmail.com>
2015-03-18 11:00:44 -0400

    added ngork, inorder to deploy the app and continue with google omni process

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
-
-  GmailAlerts::Application.routes.draw do
     root to: 'sessions#new'
     resources :sessions, only: :index
     get "/auth/:provider/callback" => 'sessions#create'
-  end
 
 
   resources :user_friends
